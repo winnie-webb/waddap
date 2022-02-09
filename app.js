a5760989163b392fc3eb113760b556e33918e46e
winnie-webb <wbrown152020@gmail.com>
2022-02-08 22:07:22 -0500

    Added basic data requirements

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const mysql2 = require("mysql2");
 const app = express();
-
+const connectDb = require("./models/ConnectDb");
+connectDb();
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
 app.use(express.json());
