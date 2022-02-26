5c3cd435de97945fc2f653ac7165c1f4d9df2efc
winnie-webb <wbrown152020@gmail.com>
2022-02-26 16:04:01 -0500

    Added placeholder to Dob picker

--- public/src/components/Signup/DobPicker.jsx
+++ public/src/components/Signup/DobPicker.jsx
@@ -9,22 +9,27 @@ function DobPicker() {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     return (
         <div className="dob-picker">
-            <select id="dob-picker__month" className="signup__form__input input-select" onChange={(e) => {
+            <select defaultValue="" id="dob-picker__month" className="signup__form__input input-select" onChange={(e) => {
                 const currentDaysInMonth = new Date(currentYear, months.indexOf(e.target.value) + 1, 0).getDate()
                 setDaysInMonth(currentDaysInMonth)
                 setMonth(months.indexOf(e.target.value))
 
             }}>
+                <option value="" disabled hidden>Month</option>
+
                 {months.map(month => <option key={month}>{month}</option>)}
             </select>
-            <select id="dob-picker__day" className="signup__form__input input-select" >
+            <select defaultValue="" id="dob-picker__day" className="signup__form__input input-select" >
+                <option value="" disabled hidden>Day</option>
                 {days.map(day => <option key={day}>{day}</option>)}
             </select>
-            <select id="dob-picker__year" className="signup__form__input input-select" onChange={(e) => {
+            <select defaultValue="" id="dob-picker__year" className="signup__form__input input-select" onChange={(e) => {
                 const newCurrentYear = parseInt(e.target.value)
                 const currentDaysInMonth = new Date(newCurrentYear, month + 1, 0).getDate();
                 setDaysInMonth(currentDaysInMonth)
             }}>
+                <option value="" disabled hidden>Year</option>
+
                 {years.map(year => <option key={year}>{year}</option>)}
             </select>
 
