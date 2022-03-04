52d8a415a1ade6e6b74d76b0f7663c9fd2da998b
winnie-webb <wbrown152020@gmail.com>
2022-03-04 16:53:35 -0500

    Added validation for all signup fields

--- public/src/components/formValidation.js
+++ public/src/components/formValidation.js
@@ -14,3 +14,83 @@ export const validateContact = (value) => {
 export const validateName = (value) => {
   if (!value) return "Required*";
 };
+
+export const validatePassword = (value) => {
+  let error;
+  const isValidLength = validator.isLength(value, {
+    min: 8,
+    max: 20,
+  });
+  if (!isValidLength) {
+    return (error = "Password must be between 8 and 20 characters long");
+  }
+  const hasLowerCase = RegExp("(?=.*[a-z])").test(value);
+  if (!hasLowerCase) {
+    return (error = "Password must contain at least 1 lowercase letter");
+  }
+  const hasUpperCase = RegExp("(?=.*[A-Z])").test(value);
+
+  if (!hasUpperCase) {
+    return (error = "Password must contain at least 1 uppercase");
+  }
+
+  const hasNumber = RegExp("(?=.*[0-9])").test(value);
+  if (!hasNumber) return (error = "Password must contain at least 1 number");
+
+  return error;
+};
+export const validateConfirmPassword = (password, value) => {
+  let error;
+  if (!value) return (error = "Required*");
+  if (password && value) {
+    if (password !== value) {
+      error = "Password does not match please check";
+    }
+  }
+  return error;
+};
+
+export const validateMonth = (value) => {
+  let error;
+  if (!value) return (error = "Required*");
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const isValid = months.indexOf(value) >= 0;
+  if (!isValid) {
+    return (error = "Invalid Month");
+  }
+};
+
+export const validateDay = (value) => {
+  let error;
+  if (!value) return (error = "Required*");
+
+  const isValid = validator.isNumeric(value);
+  if (!isValid) {
+    return (error = "Invalid Day");
+  }
+};
+
+export const validateYear = (value) => {
+  let error;
+  const yearInt = Number(value);
+  const currentYear = new Date().getFullYear();
+  if (!value) return (error = "Required*");
+  const isValid =
+    validator.isNumeric(value) && yearInt > 1900 && yearInt <= currentYear;
+  if (!isValid) {
+    return (error = "Invalid Year");
+  }
+};
